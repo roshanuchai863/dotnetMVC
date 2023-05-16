482b4cef9ea285c163d9c37437bbfb12d85b9ea5
roshanuchai863 <81355296+roshanuchai863@users.noreply.github.com>
2023-05-16 21:36:21 +1000

    file created

---
+++ Web Application/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_Application.Context;
+
+namespace Web_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCCRUDDBEntities _context = new MVCCRUDDBEntities();
+
+        public ActionResult Index()
+        {
+            var listofData = _context.Tables.ToList(); ;
+            return View(listofData);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create( Table model) {
+        _context.Tables.Add(model);
+            _context.SaveChanges();
+            ViewBag.Massage = "Data insert successful";
+            return View();
+        }
+        
+    }
+}
